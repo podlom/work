29c74d46766bcc9e09ebc689925b74e5b9054840
PetrovichevSergey <johnroget163@gmail.com>
2017-03-15 01:17:53 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Site.com</title>
+<link rel="stylesheet" href="/var/www/site.com/site.com/resources/views/style.css">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </head>
+  <body>
+
+    <div class="container">
+
+      <div class="header">
+        <header>
+          <div class="row equal">
+            <div class="col-sm-6 col-xs-6 col-lg-6 col-md-10" style="border: solid 1px black">
+
+              <div class="nav">
+                <ul class="nav nav-pills header">
+                    <li role="presentation"><a href="#">Some Text</a></li>
+                    <li role="presentation"><a href="#">Some Text</a></li>
+                    <li role="presentation"><a href="#">Some Text</a></li>
+                    <li role="presentation"><a href="#">Some Text</a></li>
+                </ul>
+            </div>
+
+            </div>
+            <div class="col-sm-6 col-xs-6 col-lg-6 col-md-2" style="border: solid 1px black">
+              <h1>Logo</h1>
+            </div>
+          </div>
+        </header>
+      </div>
+
+      <div id="search-auth-panel">
+        <div class="row">
+          <div class="col-md-4 col-sm-3" style="border: solid 1px black">
+            <h2>One</h2>
+          </div>
+          <div class="col-md-4 col-sm-3" style="border: solid 1px black">
+            <h2>Auth Two</h2>
+          </div>
+          <div class="col-md-4 col-sm-3" style="border: solid 1px black">
+            <h2>Search Three</h2>
+          </div>
+        </div>
+      </div>
+
+      <div id="wrapper">
+        <div class="row">
+          <div class="content col-md-9" style="border: solid 1px black">
+            <h3>Content</h3>
+          </div>
+          <div class="sidebar col-md-3" style="border: solid 1px black">
+            <h3>Sidebar</h3>
+          </div>
+        </div>
+      </div>
+
+      <div id="footer">
+        <div class="row">
+          <div class="col-md-2" style="border: solid 1px black">
+            <h4>Footer One</h4>
+          </div>
+          <div class="col-md-8" style="border: solid 1px black">
+            <h4>Footer Two</h4>
+          </div>
+          <div class="col-md-2" style="border: solid 1px black">
+            <h4>Footer Three</h4>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </body>
+</html>
