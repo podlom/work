9567f9832116ebc57f5806eae1194633482bf158
PetrovichevSergey <johnroget163@gmail.com>
2017-03-15 19:23:35 +0200

    New database config

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Site.com</title>
-<link rel="stylesheet" href="/var/www/site.com/site.com/resources/views/style.css">
     <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<link rel="stylesheet" href="/var/www/site.com/site.com/resources/views/style.css">
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
@@ -23,19 +24,18 @@
 
               <div class="nav">
                 <ul class="nav nav-pills header">
-                    <li role="presentation"><a href="#">Some Text</a></li>
-                    <li role="presentation"><a href="#">Some Text</a></li>
-                    <li role="presentation"><a href="#">Some Text</a></li>
-                    <li role="presentation"><a href="#">Some Text</a></li>
-                    <li role="presentation"><a href="#">Some Text</a></li>
-                    <li role="presentation"><a href="#">Some Text</a></li>
-                    <li role="presentation"><a href="#">Some Text</a></li>
+                    <li role="presentation"><a href="#">Главная</a></li>
+                    <li role="presentation"><a href="#">Политэмигация</a></li>
+                    <li role="presentation"><a href="#">Военная эмиграция</a></li>
+                    <li role="presentation"><a href="#">Наши друзья</a></li>
+                    <li role="presentation"><a href="#">Документы</a></li>
+                    <li role="presentation"><a href="#">События</a></li>
                 </ul>
             </div>
 
             </div>
             <div class="col-sm-6 col-xs-6 col-lg-6 col-md-2" style="border: solid 1px black">
-              <h5>UA Legion</h5>
+              <h6><b>UA Legion</b></h6>
             </div>
           </div>
         </header>
@@ -47,6 +47,7 @@
             <button type="submit" class="btn btn-link" name="mail-button" value="get-mail">Mail</button>
             <button type="submit" class="btn btn-link" name="facebook-button" value="facebook">Facebook</button>
             <button type="submit" class="btn btn-link" name="youtube-button" value="youtube">YouTube</button>
+            <button type="submit" class="btn btn-link" name="message-button" value="get-message">Отзыв</button>
           </div>
           <div class="col-md-3 col-sm-3" style="border: solid 1px black">
             <button type="submit" class="btn btn-link" value="auth">Войти/Зарегистрироваться</button>
@@ -60,7 +61,9 @@
 
           </div>
         </div>
-      </div><hr>
+      </div>
+
+      <hr>
 
       <div id="wrapper">
         <div class="row">
